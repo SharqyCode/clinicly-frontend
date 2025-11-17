5caf2ce91c5a489513726ba4288d79a65e349c02
abdelrhmanzenhom <73444167+abdelrhmanzenhom@users.noreply.github.com>
2025-11-17 10:07:40 +0200

    Remove PatientService.js as it is no longer needed

--- src/Api/Services/PatientService.js
+++
@@ -1,9 +0,0 @@
-// src/Api/Services/PatientService.js
-import axios from "axios";
-
-const API_URL = "http://localhost:5000/api";
-
-export const getAllPatients = async () => {
-  const res = await axios.get(`${API_URL}/patients`);
-  return res.data; // make sure backend returns {patients: [...]}
-};
