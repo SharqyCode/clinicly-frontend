7d101c138001619e8277d6f8801a10573f5b1d10
Ebraheem <pencilmagicart@gmail.com>
2025-11-18 17:15:19 +0200

    reverted merge

--- src/Api/Services/receptionistService.js
+++ src/Api/Services/receptionistService.js
@@ -19,9 +19,8 @@ export const createReceptionist = async (data) => {
 };
 
 
-import api from "../axiosInstance";
+import api from "../axiosInstance"
 
-export const getReceptionistDashboard = async () => {
-  const { data } = await api.get("/dashboard/receptionist");
-  return data
-}
+
+
+
