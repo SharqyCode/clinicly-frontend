2681730c4320ecbfcb920ea3bcd913175d845d19
unknown <mohamed.elbdry196@gmail.com>
2025-11-18 14:48:39 +0200

    Add new updates to appointments UI

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,19 @@
-// import ButtonExample from "../_themeUseExample/buttonExample";
+import ButtonExample from "../_themeUseExample/buttonExample";
 import PatietAi from '../Features/Doctor/Pages/AiPage'
 import DoctorSpecialtyAI from '../Features/Patient/Pages/AiPage'
+import MyAppointments from '../Features/Patient/Pages/MyAppointments'
+import MedicalRecords from '../Features/Patient/Pages/MedicalRecords'
 
 // Import global routing here
 function App() {
   return (
     <>
       {/* <ButtonExample /> */}
-      <PatietAi/>
+      {/* <PatietAi/> */}
       {/* <DoctorSpecialtyAI/> */}
+      <MyAppointments/>
+      {/* <MedicalRecords/> */}
+
     </>
   );
 }
